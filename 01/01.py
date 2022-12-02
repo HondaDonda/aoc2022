7132a1e2210e82f385a4241345a71adc041f652d
Josiah.Kidd <josiah@spicycrypto.ca>
2022-12-01 23:34:50 -0500

    Day #1 Solution

--- 01/01.py
+++ 01/01.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+  
+  with open('01/input') as infile:
+  highest = 0
+  current = 0
+  lst = []
+  for line in infile:
+    if len(line.strip()) == 0:
+      lst.append(current)
+      if current > highest:
+        highest = current    
+      current = 0
+    else:
+      current += int(line.strip())
+
+print(highest)
+lst = sorted(lst)
+print(lst[-1] + lst[-2] + lst[-3])
+
+
+
+
